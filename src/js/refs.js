812b785a010a4c7c1aa1991c9b782708c09514fc
AntonKobyshev <antonkobyshev@gmail.com>
2023-04-08 18:50:17 +0300

    check ended

--- src/js/refs.js
+++
@@ -1,6 +0,0 @@
-export const refs = {
-  searchForm: document.getElementById('search-form'),
-  gallery: document.querySelector('.gallery'),
-  toTopBtn: document.querySelector('.to-top'),
-  wrapper: document.querySelector('.wrapper'),
-};
