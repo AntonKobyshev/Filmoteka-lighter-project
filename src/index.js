6ca0a2847b004878befc1f7053a70dbe9ef2154f
Sergey Shmatok <trycifer@gmail.com>
2023-04-10 19:24:52 +0300

    All changed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import './sass/main.scss';
-import { authObserver } from './js/api/firebase/api';
-import {
-  showAuthorisedFields,
-  showUnauthorisedFields,
-} from './js/features/auth/authModalWindowContent';
+import './js/api/firebase/user-service';
 
-import './js/features/auth/modal';
 
-authObserver([showAuthorisedFields], [showUnauthorisedFields]);
 
-// import templateFunction from './templates/auth-modal';
-// document.body.innerHTML = templateFunction();
+
--- src/js/api/firebase/firebaseConfig.js
+++ src/js/api/firebase/firebaseConfig.js
@@ -1,10 +1,3 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-
-import {
-  getAuth
-} from 'firebase/auth';
-
 // Your web app's Firebase configuration
 export const firebaseConfig = {
   apiKey: "AIzaSyApAVPx8g9ncyoyFM9AB-VsIVQFVbZimu8",
@@ -15,14 +8,3 @@ export const firebaseConfig = {
   appId: "1:626971331589:web:4219ce873aff3fdaeba06c"
 };
 
-export const db = getFirestore(initializeApp(firebaseConfig));
-export const auth = getAuth();
-// // // // // // // // // // // // //
-// //Цей код встановлює з'єднання з додатком Firebase, базою даних Firestore та службою аутентифікації, використовуючи SDK Firebase для JavaScript.
-// Спочатку він імпортує необхідні модулі Firebase для ініціалізації додатку, доступу до бази даних Firestore та отримання служби аутентифікації.Конкретно,
-// він імпортує initializeApp з модуля firebase / app, getFirestore з модуля firebase / firestore та getAuth з модуля firebase / auth.
-// Потім він визначає об'єкт з іменем firebaseConfig, який містить інформацію конфігурації для додатку Firebase. Це включає API-ключ, домен аутентифікації, 
-// URL бази даних, ідентифікатор проекту, сховище, ID відправника повідомлень та ID додатку.
-
-// Потім код експортує дві константи: db та auth.db - це об'єкт Firestore бази даних, який ініціалізується за допомогою getFirestore та initializeApp(firebaseConfig).
-// auth - це об'єкт служби аутентифікації, який ініціалізується за допомогою getAuth()
