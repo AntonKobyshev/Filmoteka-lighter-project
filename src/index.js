9d33dcf7d318b35cf573060dfd6a3ef3ecb5a3c3
AntonKobyshev <antonkobyshev@gmail.com>
2023-04-12 16:52:53 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ import './js/api/apiService';
 import './js/api/firebase/user-service';
 import './js/movieCardsGallery';
 import './js/homeSearch';
-import './js/features/auth/loader';
+// import './js/features/auth/loader';
 import './js/pagination';
