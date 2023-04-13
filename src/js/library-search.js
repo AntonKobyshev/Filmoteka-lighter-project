386a5994033a252fb8238b246b7da744d367c50a
Mykola Fedotov <fedotovns@gmail.com>
2023-04-13 20:34:40 +0200

    inlib search is working

--- src/js/library-search.js
+++ src/js/library-search.js
@@ -2,44 +2,111 @@ import dataStorage from './api/firebase/data-storage';
 import { Notify } from 'notiflix';
 import 'notiflix/dist/notiflix-3.2.6.min.css';
 
-const data = new dataStorage({
-  queue: {},
-  watched: {},
-});
+// import renderMarkupByIds from './libRender';
+// const searchForm = document.querySelector('.header__searchlib');
+// const searchInput = document.querySelector('.header__searchlib-input');
+// const libraryList = document.querySelector('.library__container-list');
+
+// searchForm.addEventListener('submit', event => {
+//   event.preventDefault();
+//   const searchQuery = searchInput.value.trim().toLowerCase();
+
+//   const watchedIds = JSON.parse(localStorage.getItem('watched'));
+//   const queuedIds = JSON.parse(localStorage.getItem('queued'));
+
+//   const watchedMovies = Array.isArray(watchedIds) ? watchedIds : [];
+//   console.log(watchedMovies);
+//   const queuedMovies = Array.isArray(queuedIds) ? queuedIds : [];
+
+//   const movies = watchedMovies.concat(queuedMovies);
+
+//   const filteredMovies = movies.filter(id => {
+//     const title = localStorage.getItem(`title_${id}`);
+//     return title && title.toLowerCase().includes(searchQuery);
+//   });
+//   console.log(filteredMovies);
+//   libraryList.innerHTML = '';
+//   // renderMarkupByIds(filteredMovies, libraryList);
+
+//   let markup = '';
+//   filteredMovies.forEach(id => {
+//     const title = localStorage.getItem(`title_${id}`);
+//     markup += `<li>${title}</li>`;
+//   });
+
+//   libraryList.innerHTML = markup;
+// });
+
+// const searchForm = document.querySelector('.header__searchlib');
+// const searchInput = document.querySelector('.header__searchlib-input');
+// const libraryContainer = document.querySelector('.library__container-list');
+
+// searchForm.addEventListener('submit', event => {
+//   event.preventDefault();
+//   const searchQuery = searchInput.value.trim().toLowerCase();
+
+//   const watchedObj = JSON.parse(localStorage.getItem('watched')) || {};
+//   const watchedIds = Object.keys(watchedObj);
+//   // console.log(watchedObj);
+//   // console.log(watchedIds);
+//   const queuedObj = JSON.parse(localStorage.getItem('queued')) || {};
+//   const queuedIds = Object.keys(queuedObj);
+
+//   const movies = [...watchedIds, ...queuedIds];
+//   console.log(movies);
+//   const filteredMovies = movies.filter(id => {
+//     const title = localStorage.getItem(`title_${id}`);
+//     return title && title.toLowerCase().includes(searchQuery);
+//   });
+//   // console.log(filteredMovies);
+
+//   let markup = '';
+//   filteredMovies.forEach(id => {
+//     const title = localStorage.getItem(`title_${id}`);
+//     markup += `<li>${title}</li>`;
+//   });
+
+//   libraryContainer.innerHTML = markup;
+// });
+
+// import renderMarkupByIds from './libRender';
+
+// const searchForm = document.querySelector('.header__searchlib');
+// const searchInput = document.querySelector('.header__searchlib-input');
+
+// searchForm.addEventListener('submit', event => {
+//   event.preventDefault();
+//   const searchQuery = searchInput.value.trim().toLowerCase();
+
+//   const watched = JSON.parse(localStorage.getItem('watched')) || {};
+//   const queue = JSON.parse(localStorage.getItem('queue')) || {};
+
+//   const movieIds = Object.keys(watched).concat(Object.keys(queue));
+//   const filteredMovies = movieIds.filter(id => {
+//     const title = watched[id] || queue[id];
+//     return title && title.toLowerCase().includes(searchQuery);
+//   });
+
+//   renderMarkupByIds(filteredMovies);
+// });
+
+import renderMarkupByIds from './libRender';
 
 const searchForm = document.querySelector('.header__searchlib');
 const searchInput = document.querySelector('.header__searchlib-input');
-const searchResult = document.querySelector('#search-result'); //куди виводити результат пошуку???
 
 searchForm.addEventListener('submit', event => {
   event.preventDefault();
-  const query = searchInput.value.trim().toLowerCase();
+  const searchQuery = searchInput.value.trim().toLowerCase();
 
-  let queueResults = [];
-  let watchedResults = [];
+  const watched = JSON.parse(localStorage.getItem('watched')) || {};
+  const queue = JSON.parse(localStorage.getItem('queue')) || {};
 
-  for (const id in data.queue) {
-    if (data.queue[id].title.toLowerCase().includes(query)) {
-      queueResults.push(data.queue[id]);
-    }
-  }
+  const movieIds = Object.keys(watched).concat(Object.keys(queue));
+  const filteredMovies = movieIds.filter(id => {
+    const title = watched[id] || queue[id];
+    return title && title.toLowerCase().includes(searchQuery);
+  });
 
-  for (const id in data.watched) {
-    if (data.watched[id].title.toLowerCase().includes(query)) {
-      watchedResults.push(data.watched[id]);
-    }
-  }
-
-  const results = [...queueResults, ...watchedResults];
-
-  if (results.length > 0) {
-    searchResult.innerHTML = '';
-    results.forEach(result => {
-      const li = document.createElement('li');
-      li.textContent = result.title;
-      searchResult.appendChild(li);
-    });
-  } else {
-    Notify.warning('No results found');
-  }
+  renderMarkupByIds(filteredMovies);
 });
