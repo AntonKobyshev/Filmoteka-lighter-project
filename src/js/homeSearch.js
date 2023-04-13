731333017fc06ec5b204beac466effea61523729
Kriminz <artur26062004@gmail.com>
2023-04-13 19:53:59 +0300

    alt

--- src/js/homeSearch.js
+++ src/js/homeSearch.js
@@ -58,7 +58,7 @@ function renderMovie(response) {
       if (genre_ids.length > 2) {
         return `
       <li class="movie-card" data-id="${id}">
-          <img class="movie-card__poster" src="https://image.tmdb.org/t/p/w400${poster_path}" loading="lazy" />
+          <img class="movie-card__poster" src="https://image.tmdb.org/t/p/w400${poster_path}" alt="There is no poster for this movie" loading="lazy" />
           <div class="movie-card__info">
             <p class="movie-card__title">${title}</p>
             <p class="movie-card__additionaly">${genre_ids[0]},&nbsp;${genre_ids[1]},&nbsp;Other | ${releaseYear}</p>
@@ -68,10 +68,10 @@ function renderMovie(response) {
       } else {
         return `
       <li class="movie-card" data-id="${id}">
-          <img class="movie-card__poster" src="https://image.tmdb.org/t/p/w400${poster_path}" loading="lazy" />
+          <img class="movie-card__poster" src="https://image.tmdb.org/t/p/w400${poster_path}" alt="There is no poster for this movie" loading="lazy" />
           <div class="movie-card__info">
             <p class="movie-card__title">${title}</p>
-            <p class="movie-card__additionaly">${genre_ids[0]},&nbsp;${genre_ids[1]} | ${releaseYear}</p>
+            <p class="movie-card__additionaly">${genre_ids} | ${releaseYear}</p>
           </div>
       </li>
       `;
