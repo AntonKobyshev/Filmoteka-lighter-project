3dcd65595f495145ba4ded7983417dadfa0c898a
AntonKobyshev <antonkobyshev@gmail.com>
2023-04-13 18:54:16 +0300

    watch trailer reserve btn

--- src/js/features/auth/modal.js
+++ src/js/features/auth/modal.js
@@ -76,8 +76,9 @@ const modalMoviemarkup = (
 <div class="modal__buttons">
     <button type="button" class="modal__button modal__add-watched" data-watched='false' data-liery='false'>add to watched</button>
     <button type="button" class="modal__button modal__add-queue" data-queue='false' data-liery='false'>add to queue</button>
-    <button type="button" class="modal__button modal__watch-traier" data-queue='false' data-liery='false'>watch trailer</button>
 </div>
+    <button type="button" class="modal__button modal__watch-traier" data-queue='false' data-liery='false'>watch trailer</button>
+
 
 
     </div>
